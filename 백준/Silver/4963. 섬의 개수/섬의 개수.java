23819258a25b77914df522d4f4c89cd4978e8376
SOO <chzhvnemf009@gmail.com>
2025-08-11 00:52:50 +0900

    [Silver II] Title: 섬의 개수, Time: 140 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -1,67 +1,61 @@
 import java.io.*;
 import java.util.*;
-class Main{
-    static int[] ar = {-1,-1,-1,0,1,1,1,0};
-    static int[] ac = {-1,0,1,1,1,0,-1,-1};
+
+public class Main {
+	static int r = 1;
+	static int c = 1;
+	static int[][] map;
+	static int[] ar = {-1,0,1,0,-1,1,1,-1};
+	static int[] ac = {0,1,0,-1,1,1,-1,-1};
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		Boolean isEnd = false;
 		
-		while(!(isEnd)) {
+		while(r!=0) {
 			st = new StringTokenizer(br.readLine());
-			int c = Integer.parseInt(st.nextToken()); int r = Integer.parseInt(st.nextToken());
-			if(r==0) {
-				isEnd = true;
-				break;
-			}
+			c = Integer.parseInt(st.nextToken());
+			r = Integer.parseInt(st.nextToken());
+            if(r==0) break;
+			map = new int[r][c];
 			
-			int[][] land = new int[r][c];
 			for(int i=0; i<r; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0; j<c; j++) {
-					land[i][j] = Integer.parseInt(st.nextToken());
+					map[i][j] = Integer.parseInt(st.nextToken());
 				}
-			}//배열 입력 끝.
+			}//배열 받기 끝
 			
-			int cnt = 0;
-			for(int i=0; i<r; i++) {
+			int cnt =0;//섬 개수
+			for(int i=0; i<r; i++) {//섬 탐색
 				for(int j=0; j<c; j++) {
-					if(land[i][j]==1) {
-						//팔방 탐색 진입.
-						look(i,j,land,r,c);
-						cnt++;
+					if(map[i][j]==1) {
+						dfs(i,j);
+						cnt += 1;
 					}
-					
 				}
-			}//배열 탐색
+			}//섬 탐색 끝
 			
 			System.out.println(cnt);
 			
 			
-		}
-
-	}
+		}//반복 끝
+	}//메인 끝
 	
-	//탐색진행
-	static void look(int x, int y,int[][] map,int R, int C ) {
-		map[x][y] = 0; //방문 처리
+	static void dfs(int mr, int mc) {
+		map[mr][mc] =0; //원본 배열이 바뀌는 걸로 visited 대신 체크 하고 있음 ㅋㅋ
 		
-		for(int i =0; i<8; i++) {
-			int nr = x + ar[i];
-			int nc = y + ac[i];
+		for(int i=0; i<8; i++) {
+			int nr = mr + ar[i];
+			int nc = mc + ac[i];
 			
-			if(0<=nr&&nr<R && 0<=nc&&nc<C) {
-				if(map[nr][nc]==1) {
-					look(nr,nc,map,R,C);
+			if(0<=nr&&nr<r && 0<=nc&&nc<c) {
+				if(map[nr][nc] ==1) {
+					dfs(nr,nc);
 				}
 			}
-		}
+		}//사방 탐색 종료 
 		
-	}
-	
-	
-		
+	}//함수 종료
 
 }
