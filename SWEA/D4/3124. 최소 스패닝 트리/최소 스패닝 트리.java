4a7da2d9d72e0f599b901f5141dff62c32e3ffb7
SOO <chzhvnemf009@gmail.com>
2025-08-27 15:43:54 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,126 ms, Memory: 203,892 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,115 +1,105 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
-	static class Edge implements Comparable<Edge>{//간선 1개 표현
-		int from, to, weight;
-		
-		
-		public Edge(int from, int to, int weight) {
-		this.from = from;
-		this.to = to;
-		this.weight = weight;
-	}
+	static class Vertex implements Comparable<Vertex>{
+		int e;
+        long w;
 
+		public Vertex(int e, long w) {
+			this.e = e;
+			this.w = w;
+		}
 
 		@Override
-		public int compareTo(Edge o){ //나를 기준으로 상대를 생각하기
-			//10 - 20 : 음수 -> 뒤 값이 크다 : 그럼 그대로 위치(교환 일어나지 않음)
-			//20 - 20 : 양수 -> 앞 값이 크다 : 교환
-			//양수 - 음수 : 오버플로우, 음수 - 양수 : 언더플로우
-			// return this.weight - o.weight; (음수 양수 섞이지 않는 상황이라면 숫자들이)
-			return Integer.compare(this.weight,o.weight); //가중치 기준 오름차순 정렬 되도록 비교 결과 리턴
+		public int compareTo(Vertex o) { //여기는 정렬이라 -1, 0, 1이라 int 상관없음
+			return Long.compare(this.w, o.w); //여기는 계산하는 부분이라 long써야함
 		}
-		
-	}
+	}//끝 Vertex
 	
-	static Edge[] edgeList;
-	static int[] parents;
-	static int V,E;
-	
-	
+	static List<Vertex>[] lst;
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
-		
-		for(int tc =1; tc<=T; tc++) {
+		for(int tc = 1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			V = Integer.parseInt(st.nextToken());
-			E = Integer.parseInt(st.nextToken());
+			int V = Integer.parseInt(st.nextToken()); // 정점의 개수
+			int E = Integer.parseInt(st.nextToken()); //간선의 개수
 			
-			parents = new int[V+1];
-			edgeList = new Edge[E];
+			lst = new ArrayList[V+1];
 			
-			for(int i=0; i<E; i++){
+			for(int i=0; i<V+1; i++) {
+				lst[i] = new ArrayList<>();
+			}
+			
+			
+			for(int i=0; i<E; i++) {
 				st = new StringTokenizer(br.readLine());
+				int A = Integer.parseInt(st.nextToken()); //A번 정점
+				int B = Integer.parseInt(st.nextToken()); //B번 정점
+				long C = Long.parseLong(st.nextToken()); //가중치
 				
-				int from = Integer.parseInt(st.nextToken()); //정점
-				int to = Integer.parseInt(st.nextToken()); //정점
-				int weight = Integer.parseInt(st.nextToken());  //가중치
+				lst[A].add(new Vertex(B,C));
+				lst[B].add(new Vertex(A,C));
 				
-				edgeList[i] = new Edge(from, to, weight);
+			}//간선입력 끝
 			
-			}//끝 간선 입력
-			
-			Arrays.sort(edgeList);
-			make();
-			
-			long result = 0; //최소신장트리 비용
-			int cnt = 0; //처리된 간선수
-			
-			for(Edge edge:edgeList){
-				if(!union(edge.from, edge.to)) continue; //union 실패 : 사이클 발생
-				result += edge.weight;
-				if(++cnt == V-1) break;
-			}
-			
-			System.out.println("#"+tc+ " " + result);
+			long[] dist = new long[V+1]; //정점개수
 			
 			
+			boolean[] v = new boolean[V+1]; //방문배열
+			
+			dist[1] = 0;
 			
 			
-		}//끝 테스트 케이스
+			long result = 0;
+			int minVer = Integer.MAX_VALUE;
+			
+			Arrays.fill(dist, Long.MAX_VALUE);
+			
+			PriorityQueue<Vertex> PQ = new PriorityQueue<>();
+			PQ.offer(new Vertex(1,0));
+			
+			int picked = 0;
+			while(!PQ.isEmpty()) {
+				Vertex p = PQ.poll();
+				
+				if(v[p.e]) continue;
+				v[p.e] = true;
+				
+				result += p.w;
+				if(++picked == (V)) break;
+				
+				for(Vertex ver : lst[p.e]) {
+					int next = ver.e;
+					long w = ver.w;
+					
+					if(!v[next] && w <dist[next]) { //방문한 적이 없고 && 새로운 가중치가 더 작다면
+						dist[next] = w;
+						PQ.add(new Vertex(next, w));
+					}
+				}//탐색끝
+				
+				
+			}//끝 큐 while
+			
+			System.out.println("#"+tc+" " + result); //값이 안들어가고 있음...
+			
+		}//끝testcase
 		
-		
+	}//끝main
 
-
-	}//끝 메인
-	
-	static void make(){
-		for(int i=0; i<V; i++){
-			parents[i] = i;
-			}
-	}
-	
-	static int find(int a){
-		if(a==parents[a]) return a;
-		return parents[a] = find(parents[a]); //나의 조상과 동일한 레벨이 됨 (이게 뭔솔
-		
-	}
-
-	static boolean union(int a, int b){
-		int aRoot = find(a);
-		int bRoot = find(b);
-		if(aRoot == bRoot) return false; //같은 집합(짱vs짱)
-		
-		//아래가 rank관리 설명과 관련된 파트 
-		
-		//랭크 관리 아님!! 한쪽으로 치우치는 걸 방지 하기 위한 요소
-		if(aRoot>bRoot) parents[bRoot] = aRoot;
-		else parents[aRoot] =bRoot;
-		
-		return true;
-
-	}
-
-}//끝 클래스
+}//끝class
