23c695ca949a1ab09d6f8cc3a01f5b4533b3d973
SOO <chzhvnemf009@gmail.com>
2025-09-05 15:28:17 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 95 ms, Memory: 27,792 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -9,85 +11,81 @@ public class Solution {
 	private static int K;
 	private static int[][] map;
 	private static int maxH;
-	private static int length;
-	private static boolean[][] visited;
+	private static int depth;
 	
-	static int[] ar = {-1,0,1,0};
-	static int[] ac = {0,1,0,-1};
+	static int[] ar = {-1,0,1,0}; //상우하좌
+	static int[] ac = {0,1,0,-1}; //상우하좌
+	private static boolean[][] visited;
 
 	public static void main(String[] args) throws IOException {
+//		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("등산로조성1949")));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
-		
-		for(int tc = 1; tc<=T; tc++) {
+		for(int tc=1; tc<=T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			
 			map = new int[N][N];
+			visited = new boolean[N][N];
+			maxH =0; //최대 높이
 			
-			maxH = 0; //제일 높은 봉우리 
 			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				for(int j=0; j<N; j++) {
 					map[i][j] = Integer.parseInt(st.nextToken());
 					maxH = Math.max(maxH, map[i][j]);
 				}
-			}//입력 끝
+			}//ed input
 			
-			
-			length = 0; //최대 깊이(정답)
+			depth = 0;//최대 경로
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<N; j++) {
 					if(map[i][j]==maxH) {
-						visited = new boolean[N][N];
-						dfs(i,j,1,false); // 좌표, 최대 길이, 땅굴 팠는지
+						making(i,j,1,false);
 					}
 				}
-			}
+			}//ed find
 			
-			System.out.println("#"+tc+" " +length);
+			System.out.println("#"+tc+" "+depth);
 			
-			
-		}//tc 끝
+		}//ed tc
 
-	}
+	}//ed main
 
-	private static void dfs(int r, int c, int l, boolean usingk) {
+	private static void making(int r, int c, int dep, boolean usingK) {
 		
-		length = Math.max(length, l);
+		depth = Math.max(depth, dep); //최대 길이 갱신
 		visited[r][c] = true;
 		
 		for(int i=0; i<4; i++) {
-			int nr = r+ar[i];
-			int nc = c+ac[i];
+			int nr = r + ar[i];
+			int nc = c + ac[i];
 			
-			if(nr<0 || nr>=N || nc<0 || nc>=N || visited[nr][nc]) continue;
-			if(map[nr][nc]<map[r][c]) {
-				visited[nr][nc] = true;
-				dfs(nr,nc,l+1, usingk);
-			}else {
-				if(!usingk) {//아직 k를 사용하지 않았다면
-					
-					for(int k =1; k<=K; k++) { //여기에 K 맞추기
-						if(map[nr][nc] - k < map[r][c]) {
-							map[nr][nc] -=k;
-							visited[nr][nc] = true;
-							dfs(nr,nc,l+1,true);
-							map[nr][nc] +=k;
-						}
-					}
-					
-				}
+			if(nr<0||nr>=N || nc<0||nc>=N || visited[nr][nc]) continue;
+			if(map[nr][nc]<map[r][c]) {//다음 위치가 현재 위치보다 작아야 움직일 수 있음...
+				//다음 칸으로 이동
+				making(nr,nc,dep+1, usingK); //여기서 usingK를 false로 두면 이 전에 usingK를 썼는데도 또 사용해버림
 				
-			}
-			
-		}//사방탐색 끝
+			}else {
+				if(!usingK) {//아직 땅굴을 파지 않았다면
+					for(int k = 1; k<=K; k++) {
+						if(map[nr][nc] -k <map[r][c]) {//만약 한 번깎았을 때 map[r][c]보다 작다면
+							map[nr][nc] -= k;
+							making(nr,nc,dep+1,true);
+							map[nr][nc] += k; //원복
+						}
+						
+					}//ed K땅굴 파기
+				}//ed아직땅굴팠다면
+			}//ed else
+		}//ed for문
 		
 		visited[r][c] = false;
-	}
+		
+	}//ed making
 
 }
